872ad7b3c896020dabb40f59ce4fcc8ba8b701d1
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-05-22 01:00:05 -0400

    implementing chat limit

--- venv/astral_server/astral/consumers.py
+++ venv/astral_server/astral/consumers.py
@@ -5,28 +5,58 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 # private
 rooms = {}
 # sent to client
-rooms_info = []
+rooms_info = {}
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        # self.room_hash = ''
+        # if rooms[self.room_name]['room_password']:
+        #     self.room_hash = self.scope['url_route']['kwargs']['room_hash']
+
         self.room_group_name = 'chat_%s' % self.room_name
-
-        
-
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
+        # if the room exists and hash matches
+        if rooms_info[self.room_name]:
+        # and rooms[self.room_name]['room_hash'] == self.room_hash :
+            # Join room group
+            await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
+            await self.accept()
+            # add to capacity count
+            rooms_info[self.room_name]['roomCapacity'] += 1
+            # update lobby lists capacities 
+            await self.channel_layer.group_send(
+                'lobby',
+                {
+                    'type': 'lobby_update',
+                    'rooms_info': rooms_info,
+                }
+            )
+                
+        #error handle by frontend
+            
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
+        )
+        # subtract from capacity count
+        rooms_info[self.room_name]['roomCapacity'] -= 1
+        #if room becomes empty delete
+        if rooms_info[self.room_name]['roomCapacity'] == 0 :
+            rooms.pop(self.room_name)
+            rooms_info.pop(self.room_name)
+        # update lobby lists capacities 
+        await self.channel_layer.group_send(
+            'lobby',
+            {
+                'type': 'lobby_update',
+                'rooms_info': rooms_info,
+            }
         )
 
     # Receive message from WebSocket
@@ -43,13 +73,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
+    # Receive message from chat group
     async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
+        }))
+
+     # Receive message from lobby group
+    async def lobby_update(self, event):
+        updated_rooms = event['rooms_info']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'rooms': updated_rooms,
         }))
 #///////////////////////
 #///////////////////////
@@ -67,13 +106,9 @@ class LobbyConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'lobby_update',
-                'rooms_info': rooms_info,
-            }
-        )
+        await self.send(text_data=json.dumps({
+            'rooms': rooms_info
+        }))
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -89,19 +124,29 @@ class LobbyConsumer(AsyncWebsocketConsumer):
         #if client requests to join a room
         if request_type == 'join':
             room_name = text_data_json['roomName']
-            #if room has a password
-            if rooms[room_name]['room_password']:
-                #send password request message to client
+            room_capacity = rooms_info[room_name]['roomCapacity']
+            #if there's room to join
+            if room_capacity < 8:
+                #if room has a password
+                if rooms[room_name]['room_password']:
+                    #send password request message to client
+                    await self.send(text_data=json.dumps({
+                        'roomName': room_name,
+                        'request': 'password required',
+                    }))
+                #if room is public
+                else:
+                    #allow access to client
+                    await self.send(text_data=json.dumps({
+                        'roomName': room_name,
+                    })) 
+            #if room is full
+            else: 
+                #send error to client
                 await self.send(text_data=json.dumps({
-                    'roomName': room_name,
-                    'request': 'password required',
-                }))
-            #if room is public
-            else:
-                #allow access to client
-                await self.send(text_data=json.dumps({
-                    'roomName': room_name,
+                    'error': 'full capacity'
                 })) 
+
         #if client submits a private room password
         if request_type == 'password':
             room_name = text_data_json['roomName']
@@ -109,7 +154,7 @@ class LobbyConsumer(AsyncWebsocketConsumer):
             #if the password matches, return secret hash
             if rooms[room_name]['room_password'] == room_password:
                 await self.send(text_data=json.dumps({
-                    'roomName': rooms[room_name],
+                    'roomName': room_name,
                     'roomHash': rooms[room_name]['room_hash'],
                 }))
             #if the password doesnt match, return error 
@@ -117,6 +162,7 @@ class LobbyConsumer(AsyncWebsocketConsumer):
                 await self.send(text_data=json.dumps({
                     'error': 'invalid password'
                 }))
+
         #if client requests to start a room
         elif request_type == 'start':
             room_name = text_data_json['roomName']
@@ -129,23 +175,16 @@ class LobbyConsumer(AsyncWebsocketConsumer):
                     room_name : {
                         'room_password': room_password,
                         'room_hash': room_hash if room_password else '',
-                        'room_capacity': 0,
                     }
                 })
                 #add room info to rooms info list
-                rooms_info.append({
-                    'roomName': room_name,
-                    'roomAccess': 'private' if room_password else 'public',
-                    'roomCapacity': 0,
-                })
-                # Send room names to lobby group
-                await self.channel_layer.group_send(
-                    self.room_group_name,
-                    {
-                        'type': 'lobby_update',
-                        'rooms_info': rooms_info,
+                rooms_info.update({
+                    room_name : {
+                        'roomName': room_name,
+                        'roomAccess': 'private' if room_password else 'public',
+                        'roomCapacity': 0,
                     }
-                )
+                })
                 # Send room name to creator (pushes them into room)
                 await self.send(text_data=json.dumps({
                     'roomName': room_name,
--- venv/astral_server/astral/views.py
+++ venv/astral_server/astral/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'astral/index.html')
 
-def room(request, room_name, room_hash):
+def room(request, room_name, room_hash = ''):
     return render(request, 'astral/room.html', {
         'room_name': room_name,
         'room_hash': room_hash,

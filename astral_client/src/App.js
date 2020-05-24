2543403bdc5a7784a6621512fa1518afd6eaa5ba
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-05-24 16:25:20 -0400

    app component complete

--- astral_client/src/App.js
+++ astral_client/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import './App.scss'
 
 function App() {
+
+  
+  const [lobby, toggleLobby] = useState(false);
+  const [form,setForm] = useState(null);
+  const [error, setError] = useState(null);
+  const [webSocket, setWebSocket] = useState(null);
+
+  const connectLobby = async () => {
+      const lobbySocket = await new WebSocket('ws://' + 'localhost:8000'+ '/ws/astral/');
+      setWebSocket(lobbySocket);
+      toggleLobby(!lobby);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div  className="app-overlay">
+        <div>home</div>
+        <Switch>
+          <Route path={'/:hash/:name'} render={<div>chat</div>}/>
+          <Route path={'/'} render={()=> lobby ? <div>lobby</div> : <></>}/>
+        </Switch>
+        {form && <div>form</div>}
+        {error && <div>error</div>}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+  
+
+ 
